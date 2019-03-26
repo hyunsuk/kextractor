1d44b16f29746df8b87966d704f0e9c80c1bc876
Hyunsuk Lee <hs.lee.logan@gmail.com>
2019-03-27 01:13:45 +0900

    가독성을 위해 코드 개선

--- main.go
+++ main.go
@@ -11,117 +11,128 @@ import (
 	"strings"
 )
 
-var fileExtenxion string
-var filesList []string
-var filePathWatchFlag string
+var fileExt string
+var filePaths []string
+var commets map[string]string
+var ignorePaths map[string]string
 
-func makeFilesList(path string, f os.FileInfo, err error) error {
-	if !f.IsDir() && !isTestPath(path) {
-		if fileExtenxion == "" {
-			filesList = append(filesList, path)
-		} else {
-			paths := strings.Split(f.Name(), ".")
-			if paths[len(paths)-1] == fileExtenxion {
-				filesList = append(filesList, path)
-			}
+func procFile(path string, f os.FileInfo, err error) error {
+	if err != nil {
+		return err
+	}
+
+	if f.IsDir() || isIgnorePath(path) {
+		return nil
+	}
+
+	if fileExt == "" {
+		filePaths = append(filePaths, path)
+	} else {
+		v := strings.Split(f.Name(), ".")
+		if v[len(v)-1] == fileExt {
+			filePaths = append(filePaths, path)
 		}
 	}
 	return nil
 }
 
 func fileReader(path string) {
-	if file, err := os.Open(path); err == nil {
-		defer file.Close()
+	fmt.Printf("[%s] file scanning\n", path)
+	if f, err := os.Open(path); err != nil {
+		log.Fatal(err)
 
-		scanner := bufio.NewScanner(file)
-		lineNumber := 1
+	} else {
+		defer f.Close()
+
+		scanner := bufio.NewScanner(f)
+		n := 1
 
 		for scanner.Scan() {
-			lineText := scanner.Text()
-			matched, err := regexp.MatchString("\\p{Hangul}", lineText)
+			line := scanner.Text()
+			if isComment(line) {
+				n++
+				continue
+			}
 
+			matched, err := regexp.MatchString("\\p{Hangul}", line)
 			if err != nil {
 				log.Fatal(err)
 			}
-
 			if matched {
-				if !isPythonComment(lineText) && !isHTMLComment(lineText) && !isJavascriptComment(lineText) {
-					if filePathWatchFlag != path {
-						if filePathWatchFlag != "" {
-							fmt.Println()
-						}
-						filePathWatchFlag = path
-						fmt.Printf("path : %v\n", filePathWatchFlag)
-					}
-
-					fmt.Printf("%d :  %v\n", lineNumber, lineText)
-				}
+				fmt.Printf("%d :  %v\n", n, line)
 			}
-			lineNumber++
+			n++
 		}
 
-		if err = scanner.Err(); err != nil {
-			log.Printf("file scan error - %s\n", err)
-			log.Printf("error file path - %s\n", path)
+		if err := scanner.Err(); err != nil {
+			log.Printf("[%s] file scan error - %s\n", path, err)
 		}
 
-	} else {
-		log.Fatal(err)
 	}
 }
 
-func isPythonComment(s string) bool {
-	matched, err := regexp.MatchString("\\s*#\\s*", s)
-
-	if err != nil {
-		log.Fatal(err)
+func isComment(s string) bool {
+	for _, v := range commets {
+		if matched, err := regexp.MatchString(v, s); err != nil {
+			log.Fatal(err)
+		} else if matched {
+			return matched
+		}
 	}
-	return matched
+	return false
 }
 
-func isHTMLComment(s string) bool {
-	matched, err := regexp.MatchString("\\s*<!--\\s*|.*-->$", s)
-
-	if err != nil {
-		log.Fatal(err)
+func isIgnorePath(s string) bool {
+	for _, v := range ignorePaths {
+		if matched, err := regexp.MatchString(v, s); err != nil {
+			log.Fatal(err)
+		} else if matched {
+			return matched
+		}
 	}
-	return matched
+	return false
 }
 
-func isJavascriptComment(s string) bool {
-	matched, err := regexp.MatchString("\\s*[//|/*]\\s*", s)
-
-	if err != nil {
-		log.Fatal(err)
+func init() {
+	commets = map[string]string{
+		"python":     "\\s*#\\s*",
+		"html":       "\\s*<!--\\s*|.*-->$",
+		"javascript": "\\s*[//|/*]\\s*",
 	}
-	return matched
-}
-
-func isTestPath(path string) bool {
-	matched, err := regexp.MatchString("functional_test", path)
-
-	if err != nil {
-		log.Fatal(err)
+	ignorePaths = map[string]string{
+		"test_path": "functional_test",
+		"git":       ".git",
 	}
-	return matched
 }
 
 func main() {
 	flag.Parse()
-	root := flag.Arg(1)
-	fileExtenxion = flag.Arg(0)
+	dirPath := flag.Arg(1)
+	fileExt = flag.Arg(0)
 
-	if root == "" {
+	if dirPath == "" {
 		currentDir, err := os.Getwd()
 		if err != nil {
 			log.Fatal(err)
 		}
-		root = currentDir
+		dirPath = currentDir
 	}
 
-	filepath.Walk(root, makeFilesList)
+	dirInfo, err := os.Stat(dirPath)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	for _, filePath := range filesList {
+	if !dirInfo.IsDir() {
+		log.Fatalf("'%s' must be directory", dirPath)
+	}
+
+	err = filepath.Walk(dirPath, procFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, filePath := range filePaths {
 		fileReader(filePath)
 	}
 }
