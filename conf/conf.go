70508c5508ef3265c1bb4bf9a9db3987789f0d60
Hyunsuk Lee <hs.lee.logan@gmail.com>
2019-04-20 16:12:36 +0900

    곱셈 처리를 주석으로 판단하게되는 정규식 제거

--- conf/conf.go
+++ conf/conf.go
@@ -12,5 +12,5 @@ const (
 	// RegexStrToKorean ...
 	RegexStrToKorean = "\\p{Hangul}"
 	// RegexStrComments ...
-	RegexStrComments = "#|\\/\\/|\\/\\*|<\\!\\-\\-|\\*\\s|\\-\\->|\\*\\/"
+	RegexStrComments = "#|\\/\\/|\\/\\*|<\\!\\-\\-|\\-\\->|\\*\\/"
 )
