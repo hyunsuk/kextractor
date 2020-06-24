53933b56c2095857f3c4f4e58de92fd94fcbb4cd
Logan <hs.lee.logan@gmail.com>
2020-06-24 16:38:01 +0900

    불필요한 코드 정리

--- file/file_test.go
+++ file/file_test.go
@@ -13,13 +13,10 @@ func TestScanFiles(t *testing.T) {
 	match, err := regexp.Compile("\\p{Hangul}")
 	assert.NoError(t, err)
 
-	ignore, err := regexp.Compile("#")
-	assert.NoError(t, err)
-
 	beforeFn := func(filePath string) {}
 	afterFn := func(filePath string) {}
 
-	for f := range ScanFiles(paths, match, ignore, beforeFn, afterFn) {
+	for f := range ScanFiles(paths, match, nil, beforeFn, afterFn) {
 		assert.NoError(t, f.Error())
 		assert.Equal(t, 1, len(f.MatchedLines()))
 	}
