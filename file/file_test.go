7d253b891d96332bc1d21fe58fd4fde226d41507
Logan <hs.lee.logan@gmail.com>
2020-06-25 21:58:16 +0900

    file_test.go 에 ignore 가 있는 테스트 유형 추가

--- file/file_test.go
+++ file/file_test.go
@@ -1,23 +1,49 @@
 package file
 
 import (
+	"container/heap"
 	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestScanFiles(t *testing.T) {
+var beforeFn BeforeScanFunc = func(path string) {}
+var afterFn AfterScanFunc = func(path string) {}
+
+func TestScanFilesWithMatch(t *testing.T) {
 	paths := []string{"test1.md", "test2.md"}
 
 	match, err := regexp.Compile("\\p{Hangul}")
 	assert.NoError(t, err)
 
-	beforeFn := func(filePath string) {}
-	afterFn := func(filePath string) {}
-
 	for f := range ScanFiles(paths, match, nil, beforeFn, afterFn) {
 		assert.NoError(t, f.Error())
 		assert.Equal(t, 1, len(f.MatchedLines()))
 	}
+}
+
+func TestScanFilesWithMatchAndIgnore(t *testing.T) {
+	paths := []string{"test1.md", "test2.md"}
+
+	match, err := regexp.Compile("\\p{Hangul}")
+	assert.NoError(t, err)
+
+	ignore, err := regexp.Compile("#")
+	assert.NoError(t, err)
+
+	files := &Heap{}
+	heap.Init(files)
+	for f := range ScanFiles(paths, match, ignore, beforeFn, afterFn) {
+		assert.NoError(t, f.Error())
+		heap.Push(files, f)
+	}
+
+	test1File, ok := heap.Pop(files).(*File)
+	assert.True(t, ok)
+	assert.True(t, len(test1File.MatchedLines()) == 1)
+
+	test2File, ok := heap.Pop(files).(*File)
+	assert.True(t, ok)
+	assert.True(t, len(test2File.MatchedLines()) == 0)
 }
