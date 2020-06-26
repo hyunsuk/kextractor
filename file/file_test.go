28ecc3242489d0169c43ead0f800a295ee95856a
Logan <hs.lee.logan@gmail.com>
2020-06-26 12:00:00 +0900

    file 패키지 Heap 테스트 추가

--- file/file_test.go
+++ file/file_test.go
@@ -47,3 +47,31 @@ func TestScanFilesWithMatchAndIgnore(t *testing.T) {
 	assert.True(t, ok)
 	assert.True(t, len(test2File.MatchedLines()) == 0)
 }
+
+func TestHeap(t *testing.T) {
+	paths := []string{"c-test.md", "b-test.md", "a-test.md"}
+
+	match, err := regexp.Compile("\\p{Hangul}")
+	assert.NoError(t, err)
+
+	ignore, err := regexp.Compile("#")
+	assert.NoError(t, err)
+
+	files := &Heap{}
+	heap.Init(files)
+
+	for _, path := range paths {
+		f := &File{path, match, ignore, map[int][]byte{}, nil}
+		heap.Push(files, f)
+	}
+
+	file, ok := heap.Pop(files).(*File)
+	assert.True(t, ok)
+	assert.True(t, file.Path() == paths[2])
+	file, ok = heap.Pop(files).(*File)
+	assert.True(t, ok)
+	assert.True(t, file.Path() == paths[1])
+	file, ok = heap.Pop(files).(*File)
+	assert.True(t, ok)
+	assert.True(t, file.Path() == paths[0])
+}
